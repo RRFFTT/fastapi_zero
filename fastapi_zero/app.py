458f45a1a2063b391cfa0823a3bed9754009815e
Rfftt <rl.rfftt@proton.me>
2025-05-27 01:12:03 -0300

    Aula 03 - inciada.

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -3,7 +3,7 @@ from http import HTTPStatus
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
-from fastapi_zero.schemas import Message
+from fastapi_zero.schemas import Message, UserSchema, UserPublic
 
 app = FastAPI(title='My First API')
 
@@ -11,7 +11,6 @@ app = FastAPI(title='My First API')
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá, mundo!'}
-
 
 @app.get(
     '/html-response', response_class=HTMLResponse, status_code=HTTPStatus.OK
@@ -28,3 +27,11 @@ def htmlpage():
                 </body>
             </html>
             """
+
+
+@app.post('/users/',
+          status_code=HTTPStatus.CREATED,
+          response_model=UserPublic)
+
+def create_user(user:UserSchema):
+    return user
